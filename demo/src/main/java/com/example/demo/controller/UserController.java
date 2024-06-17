eda7386e6db693fb84c0a45d09843162c43f3b4c
Priyanka Khokrale <priyanka.khokrale@vnnogile.in>
2024-06-17 18:00:51 +0530

    logger added

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -2,22 +2,30 @@ package com.example.demo.controller;
 
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+//    Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
 
     @PostMapping("/adduser")
     public ResponseEntity<User> createUser(@RequestBody User user) {
+        log.info("entering the request of saving the user data");
         User createdUser = userService.createUser(user);
+        log.info("saved the data");
+
         return ResponseEntity.ok(createdUser);
     }
 
--- demo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,23 +1,35 @@
 package com.example.demo.service.impl;
 
 
+import com.example.demo.controller.UserController;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
+
+  //  Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
 
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public User createUser(User user) {
+        log.info("in service createUser, calling dao/repo");
         User savedUser = userRepository.save(user);
+        log.info("call completed");
+
         return savedUser;
     }
 
